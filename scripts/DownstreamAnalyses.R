43f51efefd7997e0f3d3cf6654ca01124b40a509
amyecampbell <amyecampbell@gmail.com>
2022-07-10 17:27:42 -0400

    Downstream

--- scripts/DownstreamAnalyses.R
+++ scripts/DownstreamAnalyses.R
@@ -74,41 +74,72 @@ FullDataDataPresentMelt$variable = (if_else(FullDataDataPresentMelt$variable=="D
 
 
 StudyIDOrder = unique((FullDataDataPresentMelt %>% arrange(woundcarepain))$study_id)
-DataAvailablePlot = ggplot(FullDataDataPresentMelt, aes(x=study_id, y=variable, fill=factor(value))) + geom_tile() + scale_fill_manual(values=c("white", "black")) + theme_classic() + theme(axis.text.x=element_text(angle=80), legend.position = "None")
+
+
+dim(FullDataDataPresentMelt %>% arrange(woundcarepain) %>% select(woundcarepain, study_id) %>% unique())
+DataAvailablePlot = ggplot(FullDataDataPresentMelt, aes(x=study_id, y=variable, fill=factor(value))) + geom_tile() + scale_fill_manual(values=c("white", "black")) + theme_classic() + theme(axis.text.x=element_text(angle=80, size=4), legend.position = "None")
 DataAvailablePlot$data$study_id = factor(DataAvailablePlot$data$study_id, levels=StudyIDOrder)
 DataAvailablePlot$data$variable = factor(DataAvailablePlot$data$variable , levels= c("Microbiome", setdiff(DataAvailablePlot$data$variable, "Microbiome")))
 ggsave(DataAvailablePlot + theme(axis.text.x=element_blank()) + xlab("Patient") + ylab("Biological Variable"), file="~/Documents/IowaWoundData2021/PaperFigs/DataPresence.pdf", width=20, height=4)
 ggsave(DataAvailablePlot + xlab("Patient") + ylab("Biological Variable"), file="~/Documents/IowaWoundData2021/PaperFigs/DataPresencePatientIDs.pdf", width=20, height=4)
 
-View(FullDataDataPresentMelt %>% arrange(woundcarepain) %>% select(study_id, woundcarepain) %>% unique())
 
 
-CytokineDataNonNA$Ct.Tech.Mean-CytokineDataNonNA$Delta.Ct.Mean
-
-NumSamples = table(CytokineDataNonNA$Target.Name)
-
-CytokineData100Plus = CytokineData %>% filter(Target.Name %in% names(NumSamples)[NumSamples>100]) %>% left_join(WoundMicrobiome, by="X")
-
-
-ggplot(CytokineData100Plus, aes(x=Delta.Ct.Mean)) + geom_histogram() + facet_grid(.~Target.Na)
-
-unique(CytokineData100Plus$Target.Name)
-
-CXCL8Hs00174103_m1Data = CytokineData100Plus %>% filter(Target.Name=="CXCL8-Hs00174103_m1")
+# Log-fold changes for Moderate/Severe vs. Mild/None? the deltadeltaCT method?
+# Test for differences, visualize that way if we have something to show
+###############################################################################
+FullData = FullData %>% left_join(ClinicalData %>% select(study_id, woundcarepain))
+FullData = FullData %>% mutate(PainCatBinary = case_when( woundcarepain==0 | woundcarepain==1 | woundcarepain==2 ~"NoneMildModerate",
+                                                           woundcarepain==3~ "Severe", 
+                                                          ))
 
 
-summary(aov(Delta.Ct.Mean~DMMClusterAssign, data=CXCL8Hs00174103_m1Data))
-summary(aov(Delta.Ct.Mean~DMMClusterAssign, data=CXCL8Hs00174103_m1Data))
+FullData$PainCatBinary[FullData$PainCatBinary=="NA"] = NA
 
-LCN2Hs01008571_m1data = CytokineData100Plus %>% filter(Target.Name=="LCN2-Hs01008571_m1") 
-summary(aov(Delta.Ct.Mean~DMMClusterAssign, data=LCN2Hs01008571_m1data))
-ggplot(LCN2Hs01008571_m1data, aes(x=factor(DMMClusterAssign), y=Delta.Ct.Mean))  + geom_boxplot()
+listCytokines = c("ARG1-Hs00163660_m1",  "C3-Hs00163811_m1",  "C5AR1-Hs00704891_s1", "CAMP-Hs00189038_m1",  "CXCL8-Hs00174103_m1", "IL1A-Hs00174092_m1",  "IL1B-Hs01555410_m1",
+"IL6-Hs00174131_m1", "LCN2-Hs01008571_m1", "MMP1-Hs00899658_m1", "MMP2-Hs01548727_m1", "MMP9-Hs00957562_m1", "TNF-Hs00174128_m1")
 
 
+listPvalues = c()
+listcytokinesTested = c()
+listPvaluesNoneVsSevere = c()
+for(cyt in listCytokines){
+  testresult = (wilcox.test(FullData[,cyt] ~ FullData[,"PainCatBinary"]))
+  listcytokinesTested = c(listcytokinesTested, cyt)
+  listPvalues = c(listPvalues, testresult$p.value)
+  
+}
 
-# Log-fold changes for Moderate/Severe vs. Mild/None? the deltadeltaCT method? 
-####################################################
 
+
+
+
+
+DataFrameCytokinePain = data.frame(pvals = listPvalues, Cytokine=listcytokinesTested)
+DataFrameCytokinePain$PAdjust = p.adjust(DataFrameCytokinePain$pvals, method="BH")
+
+# Ugh how to calculate Fold change 
+####################################
+geo_mean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
+for(cyt in listCytokines){
+  CtrlValues = FullData[, c(cyt, "PainCatBinary")] %>% filter(PainCatBinary=="NoneMildModerate") 
+  print(CtrlValues)
+  FullData[, paste0(cyt, "_CtrlMean")]= geo_mean(CtrlValues[,cyt])
+  FullData[, paste0(cyt, "_TwoDeltaDeltaCT")] = 2^(-( (FullData[, cyt]) - (FullData[, paste0(cyt, "_CtrlMean")])))
+  
+
+}
+
+View(FullData)
+View(FullData %>% select(`IL6-Hs00174131_m1_CtrlMean`, PainCatBinary, `IL6-Hs00174131_m1_TwoDeltaDeltaCT`, `IL6-Hs00174131_m1`))
+
+log2(mean((FullData %>% filter(PainCatBinary!="NoneMildModerate"))$`IL1B-Hs01555410_m1_TwoDeltaDeltaCT`, na.rm=T) /  mean((FullData %>% filter(PainCatBinary=="NoneMildModerate"))$`IL1B-Hs01555410_m1_TwoDeltaDeltaCT`, na.rm=T))
+log2(mean((FullData %>% filter(PainCatBinary!="NoneMildModerate"))$`IL6-Hs00174131_m1_TwoDeltaDeltaCT`, na.rm=T) /  mean((FullData %>% filter(PainCatBinary=="NoneMildModerate"))$`IL6-Hs00174131_m1_TwoDeltaDeltaCT`, na.rm=T))
+log2(mean((FullData %>% filter(PainCatBinary!="NoneMildModerate"))$`IL1B-Hs01555410_m1_TwoDeltaDeltaCT`, na.rm=T) /  mean((FullData %>% filter(PainCatBinary=="NoneMildModerate"))$`IL1B-Hs01555410_m1_TwoDeltaDeltaCT`, na.rm=T))
+log2(mean((FullData %>% filter(PainCatBinary!="NoneMildModerate"))$`C5AR1-Hs00704891_s1_TwoDeltaDeltaCT`, na.rm=T) /  mean((FullData %>% filter(PainCatBinary=="NoneMildModerate"))$`C5AR1-Hs00704891_s1_TwoDeltaDeltaCT`, na.rm=T))
 
 
 
